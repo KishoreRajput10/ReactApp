bb77ad8d916119a02df4789eaac9f59fedaad9f7
KishoreRajput10 <kishoresinghrajput66@gmail.com>
2017-07-19 04:35:13 +0530

    Search Functionality implemented on table

--- contact.jsx
+++ contact.jsx
@@ -13,19 +13,15 @@ class Contact extends React.Component {
         var getData = JSON.parse(localStorage.getItem(('myData')))
 
         console.log("data")
-        console.log(data12)
-        this.state.data1 = getData;
-
+       
+        
+         this.state.items = getData, //declare the items to be listed
+      this.statesearchTerm = null //initial search term will be null
+      this.onChange = this.onChange.bind(this)
     }
 
     componentWillMount() {
         console.log("123")
-        //call the first time the component is loaded right before the component is added to the page
-        //this.search();
-        // for(var i=0; i<data12.length;i++){
-        //     console.log('kishore')
-        // }
-        // console.log(data12.lenght)
     }
     componentDidMount() {
         console.log("321")
@@ -43,69 +39,61 @@ class Contact extends React.Component {
         // called when the component is removed
     }
 
-    updateSearch() {
-        var myQuery = this.refs.query.value;
-
-        
-        var getData = JSON.parse(localStorage.getItem(('myData')));
-
-        // var regex = new RegExp("^(.*)"+(myQuery)+"(.*$)","ig");
-        
-
-         var regex = new RegExp("^(.*)" + (myQuery) + "(.*$)","ig");
-
-        var query = {name : myQuery};
-
-
-       console.log(query)
-
-        var result = _.findWhere(getData, query);
-
-        console.log(result)
-
-        var arr = [];
-        arr.push(result)
-
-        this.setState({ data1 : arr})
+    // search(query) {
+    //     //console.log(query)
        
-
-        //this.search(this.refs.query.value);
-
-    }
-
-    search(query) {
-        //console.log(query)
-       
-        var mythis = this
-         mythis.setState({ name: query })
+    //     var mythis = this
+    //      mythis.setState({ name: query })
 
 
-        /*var mydata = axios({
-            method: 'POST',
-            url: 'http://172.20.20.139:3000/api/lms/get_usrs',
-            headers: { 'X-Requested-With': 'XMLHttpRequest' },
-            responseType: 'application/json'
-        })
+    //     /*var mydata = axios({
+    //         method: 'POST',
+    //         url: 'http://172.20.20.139:3000/api/lms/get_usrs',
+    //         headers: { 'X-Requested-With': 'XMLHttpRequest' },
+    //         responseType: 'application/json'
+    //     })
 
-        mydata.then(function (response) {
-            console.log(response.data);
-            mythis.setState({ myData: response.data })
-        })
+    //     mydata.then(function (response) {
+    //         console.log(response.data);
+    //         mythis.setState({ myData: response.data })
+    //     })
 
-        mydata.catch(function (error) {
-            console.log(error)
-        })*/
+    //     mydata.catch(function (error) {
+    //         console.log(error)
+    //     })*/
         
-    }
-    clearBox(){
-        this.refs.query.value = '';
-        this.setState({ name: this.refs.query.value })
-    }
+    // }
+
+    onChange(e){
+    this.setState({
+      searchTerm: e.target.value           //set the new serah term to the state
+    })
+  }
     render() {
+        const items = this.state.items.map((item, index)=>{
+        if(!this.state.searchTerm){ 
+
+            return  <tr key={index}>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{index+ 1}</td>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{item.name}</td>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{item.email}</td>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{item.phone}</td>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{item.comment}</td>
+                    </tr> 
+
+        }                                                      
+        const regEx = new RegExp(this.state.searchTerm, 'ig')  
+        return (regEx.exec(item.name) || regEx.exec(item.phone)) && <tr key={index}>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{index+ 1}</td>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{item.name}</td>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{item.email}</td>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{item.phone}</td>
+                        <td style={{borderRight : '1px solid black',padding:'5px'}}>{item.comment}</td>
+                    </tr> 
+        })
         return <div>
             <h1>Search Contacts </h1>
-            <input ref="query" onChange={(e) => { this.updateSearch();}} type="text" />
-            {/*<button onClick={this.clearBox.bind(this)}>Clear</button>*/}
+             <input type="text" onChange={this.onChange} placeholder='search'/>
             <br/>
             <br/>
             <table style={{border:'1px solid black'}}>
@@ -119,39 +107,12 @@ class Contact extends React.Component {
                     </tr>
                 </thead>
                 <tbody style={{backgroundColor : '#ffccff'}}>
-
-                     {this.state.data1.map((data, index) =>
-                       
-                              <tr key={index}>
-                                    <td style={{borderRight : '1px solid black',padding:'5px'}}>{index+ 1}</td>
-                                    <td style={{borderRight : '1px solid black',padding:'5px'}}>{data.name}</td>
-                                    <td style={{borderRight : '1px solid black',padding:'5px'}}>{data.email}</td>
-                                    <td style={{borderRight : '1px solid black',padding:'5px'}}>{data.phone}</td>
-                                    <td style={{borderRight : '1px solid black',padding:'5px'}}>{data.comment}</td>
-                                </tr>
-                    )}
-                  
-                </tbody>
+                    {items}
+                    
+                  </tbody>
             </table>
-           
-            {<ChildName name= {this.state.name} clearBox2 = {this.clearBox.bind(this)}/>}
-            
         </div>;
     }
 }
 
-
-class ChildName extends React.Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-       return(
-            <div>
-                 <button onClick={this.props.clearBox2}>Clear</button>
-                {this.props.name}
-            </div>
-       )
-    }
-}
 export default Contact;
